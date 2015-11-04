11fea137490419b3cd3031a0faab0a73741563c8
Alexandre Bodelot <alexandre.bodelot@arkena.com>
2015-11-04 18:56:33 +0100

    Fix api/environments#index API

--- app/controllers/api/environments_controller.rb
+++ app/controllers/api/environments_controller.rb
@@ -1,10 +1,13 @@
 class Api::EnvironmentsController < ApplicationController
 
   def index
-    environments = Environments.all
+    environments = Environment.all
+    api_response(:environments => environments)
   end
 
   def update
+    env = Environment.find_by_slug(params[:id])
+    api_response(:environment => env)
   end
 
   def show
@@ -14,18 +17,18 @@ class Api::EnvironmentsController < ApplicationController
       env.inherited_variables.each do |var|
         hash[var.key] = var.value
       end
-      response = { :config => hash }
+      api_response(:config => hash)
     else
-      response = { :error => "No env named #{params[:id]}" }
+      api_response(:error => "No env named #{params[:id]}")
     end
+  end
 
+  protected
+
+  def api_response(data)
     respond_to do |format|
-      format.json {
-        render :json => response
-      }
-      format.xml {
-        render :xml => response
-      }
+      format.json { render :json => data }
+      format.xml { render :xml => data }
     end
   end
 end
